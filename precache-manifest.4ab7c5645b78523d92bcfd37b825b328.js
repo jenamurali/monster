f921f57c7338a8f0cbc139c6f3df50d1ed32102b
jenamurali <39160183+jenamurali@users.noreply.github.com>
2019-11-23 22:33:14 +0530

    Updates

---
+++ precache-manifest.4ab7c5645b78523d92bcfd37b825b328.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cbbcee7138967a419be83e85c2fe412",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d4da20d70114bf2fecc3",
+    "url": "/monsters-rolodex/static/css/main.1d535594.chunk.css"
+  },
+  {
+    "revision": "4e41f9f0e284ed6b079b",
+    "url": "/monsters-rolodex/static/js/2.76ec8fcb.chunk.js"
+  },
+  {
+    "revision": "d4da20d70114bf2fecc3",
+    "url": "/monsters-rolodex/static/js/main.be3516ba.chunk.js"
+  },
+  {
+    "revision": "90fb6e39b89555bc1a02",
+    "url": "/monsters-rolodex/static/js/runtime-main.20f8f9dd.js"
+  }
+]);
