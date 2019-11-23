9a23aecfba736a875407957d96bb2564840e8489
jenamurali <39160183+jenamurali@users.noreply.github.com>
2019-11-23 21:02:08 +0530

    ADD files to git hub to deploy

---
+++ src/component/card-list/card-list.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './card-list.styles.css'
+import {Card} from "../card/card.component"
+
+export const CardList = (props) => (
+    <div className="card-list">
+        {props.monsters.map((monster, idx) => 
+            <Card key={idx} monster={monster} />
+        )}
+    </div>
+)
